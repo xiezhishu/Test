f20b2b31af0080ab87af11b8add8f831bfa0e2a3
shu <zhishu_xie@163.com>
2018-04-21 23:22:22 +0800

    Update Test.java

--- src/com/zs/test/Test.java
+++ src/com/zs/test/Test.java
@@ -2,7 +2,7 @@ package com.zs.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello word!");
+        System.out.println("hello!");
         System.out.println("hello spark");
     }
 }
