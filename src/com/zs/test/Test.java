4d85ac4f65127f05b5b57ea384d52a045d72d268
xiezhishu <zhishu_xie@163.com>
2018-05-15 22:47:27 +0800

    test

--- src/com/zs/test/Test.java
+++ src/com/zs/test/Test.java
@@ -3,7 +3,7 @@ package com.zs.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello!");
-        System.out.println("hello spark");
-        System.out.println("h www");
+        System.out.println("hello hu spark");
+        System.out.println("h swww");
     }
 }
