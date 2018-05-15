c01572e6e1c226a8abaf2c37472a0a1a071ff22c
shu <zhishu_xie@163.com>
2018-05-15 22:44:23 +0800

    Update Test.java

--- src/com/zs/test/Test.java
+++ src/com/zs/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hello!");
         System.out.println("hello spark");
         System.out.println("h www");
+        System.out.println("flink!");
     }
 }
