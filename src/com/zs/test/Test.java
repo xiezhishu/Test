837a71838e2908e32ad2c965a3e498fee8dd1089
xiezhishu <zhishu_xie@163.com>
2018-05-17 07:23:18 +0800

    Merge branch 'shu'
    # Conflicts:
    #	src/com/zs/test/Test.java

--- src/com/zs/test/Test.java
+++ src/com/zs/test/Test.java
@@ -3,7 +3,7 @@ package com.zs.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello!");
-        System.out.println("hello hu spark");
+        System.out.println("hello hu hadoop");
         System.out.println("h vvv www");
     }
 
